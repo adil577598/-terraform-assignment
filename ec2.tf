a85babcc3dc8959e82a7b983e29d0d3d0e317d5e
adil577598 <adilbasha188@gmail.com>
2026-03-17 17:34:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "myec2" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
 
+  subnet_id = "subnet-1234567890abcdef0"
+
   tags = {
     Name = "Terraform-EC2"
   }
